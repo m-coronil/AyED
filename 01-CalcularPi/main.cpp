8958832ef0aaa1de0c2421b79b470e857d1aa2b2
m-coronil <135237279+m-coronil@users.noreply.github.com>
2023-06-30 22:41:03 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -3,25 +3,26 @@
 
 using namespace std;
 
-double result(double i=1000000)
+double calc_pi (double i)
 {
-    double calc=0;
+    float calc=0;
     int n=0;
 
     for (n; (n < i); n++){
-        n=n+1;
         if((n %2) == 0){
-           calc= calc +(1 / (2*n+1));     
+           calc= calc +(1.0 / (2*n+1));     
         }
         else      
-            calc = calc+ (1 /(2*n+1));
+            calc = calc -(1.0 /(2*n+1));
         }  
     return  4*calc;
 }
 
 int main(){
-      
-    cout  << setprecision(7) << result << endl;
+    float num;
+    double i=1000000
+    num= calc_pi(i)
+    cout  << "El número es: " << setprecision(7) << num << endl;
     
     return 0;
 }
